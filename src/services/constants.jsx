e676f7aa1fbd05e7d42c5a8a2fb347e57186b2f1
anirudhvivekpandit <anirudhp1999@gmail.com>
2025-01-21 17:01:50 +0530

    ui

--- src/services/constants.jsx
+++ src/services/constants.jsx
@@ -1,3 +1,3 @@
-export const baseUrl = "https://localhost:5253/";
+export const baseUrl = "https://0532-2405-201-18-3816-aca0-7e2-30e2-d924.ngrok-free.app/";
 export const categoryEndpoint = "api/Category";
 export const submissionEndpoint = "api/Submission";
